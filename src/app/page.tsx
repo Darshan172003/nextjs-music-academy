1d130254fb7f8327a2e3057e952b81a9a879056b
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2025-04-13 19:21:37 +0530

    Add HeroSection and Spotlight components; implement animated button with moving border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-// import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.02]">
-      <div className="flex items-center justify-center h-screen dark text-4xl text-center text-white">Learning NextJs</div>
+      <HeroSection />
     </main>
   );
 }
